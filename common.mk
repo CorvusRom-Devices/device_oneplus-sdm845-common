9238399958af0953d00ec1e6d7b73c56de7e2058
DennySPB <dennyspb@gmail.com>
2020-02-16 13:23:03 +0000

    sdm845-common: Add OnePlus Camera Support
    - Introduce a new boot receiver that force camera client package name via HIDL HAL to fix portrait mode
    - dynamicaly set packageName
    - Inherit camera & gallery packages from vendor/oneplus/camera
    
    Change-Id: Ie76e1a38b9fde0c78194fa3c47b8b239bb6f507f
    Co-authored-by: Vachounet <vachounet@live.fr>
    Signed-off-by: AnierinB <anierinb@evolution-x.org>

--- common.mk
+++ common.mk
@@ -20,6 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sdm845-common/sdm845-common-vendor.mk)
 
+# Inherit packages from vendor/addons/oneplus/camera
+$(call inherit-product, vendor/addons/oneplus/camera/config.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
@@ -89,8 +92,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/prebuilts/etc/permissions/com.google.android.GoogleCamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.GoogleCamera.xml
 
+# Camera HIDL
+    vendor.oneplus.camera.CameraHIDL@1.0 \
+    vendor.oneplus.camera.CameraHIDL@1.0-adapter-helper \
+    vendor.oneplus.camera.CameraHIDL-V1.0-java
+
 # Common init scripts
 PRODUCT_PACKAGES += \
+    init.opcamera.rc \
     init.qcom.rc \
     init.recovery.qcom.rc \
     ueventd.qcom.rc
