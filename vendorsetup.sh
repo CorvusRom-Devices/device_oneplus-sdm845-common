0b2aa12ddd6bb57fb0fddf93acb90b558f514c60
chandu078 <chandudyavanapelli03@gmail.com>
2021-01-14 10:21:12 +0000

    sdm845-common: vendorsetup: Track HAL's from YAAP
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,6 @@ rm -rf hardware/qcom/sdm845/thermal
 rm -rf hardware/qcom/sdm845/data/ipacfg-mgr/hal
 rm -rf hardware/qcom/sdm845/data/ipacfg-mgr/ipacm/src
 rm -rf hardware/qcom/sdm845/data/ipacfg-mgr/ipanat/src
-git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-18.1-caf-sdm845 hardware/qcom-caf/sdm845/display
-git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-18.1-caf-sdm845 hardware/qcom-caf/sdm845/media
-git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-18.1-caf-sdm845 hardware/qcom-caf/sdm845/audio
+git clone https://github.com/yaap/hardware_qcom-caf_sdm845_display.git -b eleven  hardware/qcom-caf/sdm845/display
+git clone https://github.com/yaap/hardware_qcom-caf_sdm845_media.git -b eleven  hardware/qcom-caf/sdm845/media
+git clone https://github.com/yaap/hardware_qcom-caf_sdm845_audio.git -b eleven  hardware/qcom-caf/sdm845/audio
