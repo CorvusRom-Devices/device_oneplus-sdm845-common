5963814d49a0290fa83e41b2e2aaf0aff4e87a7c
chandu078 <chandudyavanapelli03@gmail.com>
2021-01-14 12:31:39 +0000

    sdm845-common: vendorsetup: Track packages/apps/TouchGestures repo
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,7 @@ rm -rf hardware/qcom/sdm845/data/ipacfg-mgr/ipanat/src
 git clone https://github.com/yaap/hardware_qcom-caf_sdm845_display.git -b eleven  hardware/qcom-caf/sdm845/display
 git clone https://github.com/yaap/hardware_qcom-caf_sdm845_media.git -b eleven  hardware/qcom-caf/sdm845/media
 git clone https://github.com/yaap/hardware_qcom-caf_sdm845_audio.git -b eleven  hardware/qcom-caf/sdm845/audio
+git clone https://github.com/chandu078/packages_apps_TouchGestures.git -b 11 packages/apps/TouchGestures
 cd packages/apps/Settings
 git remote add tgs https://github.com/chandu078/android_packages_apps_Settings-1
 git fetch tgs
