b16ef73bd969601727d1569f0f8a775858aecdb7
chandu078 <chandudyavanapelli03@gmail.com>
2021-01-14 12:23:32 +0000

    sdm845-common: vendorsetup: Pick Touchgestures preference from packages/apps/Settings
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,3 +5,8 @@ rm -rf hardware/qcom/sdm845/data/ipacfg-mgr/ipanat/src
 git clone https://github.com/yaap/hardware_qcom-caf_sdm845_display.git -b eleven  hardware/qcom-caf/sdm845/display
 git clone https://github.com/yaap/hardware_qcom-caf_sdm845_media.git -b eleven  hardware/qcom-caf/sdm845/media
 git clone https://github.com/yaap/hardware_qcom-caf_sdm845_audio.git -b eleven  hardware/qcom-caf/sdm845/audio
+cd packages/apps/Settings
+git remote add tgs https://github.com/chandu078/android_packages_apps_Settings-1
+git fetch tgs
+git cherry-pick 32b5801be816bde050160f25b4b48f4011b9dc62
+cd ../../..
