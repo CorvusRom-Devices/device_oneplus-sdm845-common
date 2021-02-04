8e20dbec4d2612f8b820992029809376f48e331e
chandu078 <chandudyavanapelli03@gmail.com>
2021-02-04 06:38:17 +0000

    sdm845-common: vendorsetup: fixup!
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,13 @@
+echo "deleting duplicate repos"
 rm -rf hardware/qcom/sdm845/thermal
 rm -rf hardware/qcom/sdm845/data/ipacfg-mgr/hal
 rm -rf hardware/qcom/sdm845/data/ipacfg-mgr/ipacm/src
 rm -rf hardware/qcom/sdm845/data/ipacfg-mgr/ipanat/src
-git clone https://github.com/yaap/hardware_qcom-caf_sdm845_display.git -b eleven  hardware/qcom-caf/sdm845/display
-git clone https://github.com/yaap/hardware_qcom-caf_sdm845_media.git -b eleven  hardware/qcom-caf/sdm845/media
-git clone https://github.com/yaap/hardware_qcom-caf_sdm845_audio.git -b eleven  hardware/qcom-caf/sdm845/audio
+rm -rf hardware/qcom-caf/sdm845/display
+rm -rf hardware/qcom-caf/sdm845/media
+rm -rf hardware/qcom-caf/sdm845/audio
+
+echo "cloning sdm845 HAL's from YAAP"
+git clone https://github.com/yaap/hardware_qcom-caf_sdm845_display.git -b eleven hardware/qcom-caf/sdm845/display
+git clone https://github.com/yaap/hardware_qcom-caf_sdm845_media.git -b eleven hardware/qcom-caf/sdm845/media
+git clone https://github.com/yaap/hardware_qcom-caf_sdm845_audio.git -b eleven hardware/qcom-caf/sdm845/audio
