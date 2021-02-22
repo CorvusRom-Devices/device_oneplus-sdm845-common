696b1298a5c0a246702273ad3024cd12a5988fab
chandu078 <chandudyavanapelli03@gmail.com>
2021-02-22 05:15:03 +0000

    sdm845-common: Removepackages: Remove Some Prebuilt Apps * Gmail * arcore * Photos * RecorderPrebuilt * Maps * Email * PhotoTable * SnapdragonGallery
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := NfcNci GalleryGoPrebuilt OnePlusCamera
+LOCAL_OVERRIDES_PACKAGES := NfcNci GalleryGoPrebuilt OnePlusCamera PrebuiltGmail arcore Photos Email PhotoTable RecorderPrebuilt SnapdragonGallery Maps
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
