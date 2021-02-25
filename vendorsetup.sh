49de88326e02eb17ffe6ddce5028ddee572bf50f
chandu078 <chandudyavanapelli03@gmail.com>
2021-02-25 04:52:22 +0000

    sdm845-common: vendorsetup: Track media & audio HAL's from LineageOS
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,7 +7,8 @@ rm -rf hardware/qcom-caf/sdm845/display
 rm -rf hardware/qcom-caf/sdm845/media
 rm -rf hardware/qcom-caf/sdm845/audio
 
-echo "cloning sdm845 HAL's from YAAP"
+echo "cloning sdm845 display HAL's from YAAP"
 git clone https://github.com/yaap/hardware_qcom-caf_sdm845_display.git -b eleven hardware/qcom-caf/sdm845/display
-git clone https://github.com/yaap/hardware_qcom-caf_sdm845_media.git -b eleven hardware/qcom-caf/sdm845/media
-git clone https://github.com/yaap/hardware_qcom-caf_sdm845_audio.git -b eleven hardware/qcom-caf/sdm845/audio
+echo "cloning sdm845 media & audio HAL's from LineageOS"
+git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-18.1-caf-sdm845 hardware/qcom-caf/sdm845/media
+git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-18.1-caf-sdm845 hardware/qcom-caf/sdm845/audio
